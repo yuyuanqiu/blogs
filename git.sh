ad7a6df2495775aafddccb5f7a01a714975c2cb4
qiujida <jousindea@163.com>
2021-02-26 14:33:56 +0800

    update 2021-02-26 14:33:44

---
+++ git.sh
@@ -0,0 +1,25 @@
+git status
+
+time=$(date "+%Y-%m-%d %H:%M:%S")
+
+commits="
+update ${time}
+"
+
+echo $commits
+
+read -p "输入y继续，否则退出: " flag
+
+y="y"
+
+if [[ "$flag" == "$y" ]]
+then
+  git add --all
+  git commit -m "$commits"
+  git push origin dev_qiu
+  echo -e '++++++++++++++++++++++++++++++++\n\n\n\n\n\n'
+  git status
+  echo -e '\n\n\n\n\n\n++++++++++++++++++++++++++++++++'
+else
+  echo -e '\n\n\n\n\n\n没有做任何操作\n\n\n\n\n\n'
+fi
