cfa1c8f4fd65c69f2d6960da29f332eddeaee061
qiujida <jousindea@163.com>
2021-02-26 14:35:49 +0800

    update 2021-02-26 14:35:45

--- git.sh
+++ git.sh
@@ -16,7 +16,7 @@ if [[ "$flag" == "$y" ]]
 then
   git add --all
   git commit -m "$commits"
-  git push origin dev_qiu
+  git push origin master
   echo -e '++++++++++++++++++++++++++++++++\n\n\n\n\n\n'
   git status
   echo -e '\n\n\n\n\n\n++++++++++++++++++++++++++++++++'
